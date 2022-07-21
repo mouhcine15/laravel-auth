d7a72d3f67e756c45843b39aae80e19a981e9af6
mouhcine <mouhcine15.00@gmail.com>
2022-07-21 16:32:16 +0200

    collegamento css e js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,13 +11,14 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/back.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/back.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
