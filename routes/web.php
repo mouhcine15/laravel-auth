0c49746b542a7096129f48c5e2709680bc1b4ddf
mouhcine <mouhcine15.00@gmail.com>
2022-07-21 19:01:59 +0200

    authentication

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/admin', 'HomeController@index')->name('admin');
+// Route::resource('posts', 'Admin\PostController');
+
+Route::middleware('auth')
+    ->namespace('Admin')
+    ->name('admin.')
+    ->prefix('admin')
+    ->group(function () {
+        Route::get('/', 'HomeController@index')->name('dashboard');
+        Route::resource('posts', 'PostController');
+    });
